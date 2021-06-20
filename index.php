7662a9c12eeb4d07339a6eea944b2f426aaea3c5
Mondy Fathy <50025676+mahmoudMFathy@users.noreply.github.com>
2021-06-20 21:22:23 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="design/assets/indexstyle.css">
+    <title>my proj</title>
+</head>
+
+<body>
+    <header id="headerindex">
+        <p>
+            <marquee width="70%" behavior="scroll" direction="down" scrollamount="4" style="text-align: center;">welcome in my proj </marquee>
+            <marquee width="70%" behavior="scroll" direction="up" scrollamount="4" style="text-align: center;">welcome in my proj </marquee>
+        </p>
+    </header>
+    <section id="indexsec">
+        <p>
+            you already amember
+            <a href="login.php"> login</a>
+
+        </p>
+        you don't have account
+        <a href="reg.php"> register</a>
+    </section>
+    <footer id="footerindex">
+        <p>&copy;&nbsp;design by mondy&nbsp;&bull; </p>
+    </footer>
+</body>
+
+</html>
